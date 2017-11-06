34aa43ec4a821e1c4449bae727efedf89196ac87
Alex Waychoff <AlexWaychoff@gmail.com>
2017-11-06 10:26:50 -0600

    Added searchByName for case 'yes' under function app

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function app(people){
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch(searchType){
     case 'yes':
-    // TODO: search by name
-    break;
+		searchByName(people);			// TODO: search by name
+		break;
     case 'no':
     searchByTraits(people);
     break;
